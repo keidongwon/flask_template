04824ca4c886f0420522c9f39e70b2ea2b3d59a6
keidongwon <keidongwon@gmail.com>
2018-04-21 21:00:28 +0900

    * 예외처리 변경

--- src/runserver.py
+++ src/runserver.py
@@ -1,12 +1,9 @@
+import sys
 import logging
 from flask import Flask
 from flask_restful import Api
-from common.constant import FilePrefix
-from common.constant import SystemString
-from common import preload
-from common import config
-from common import errorcode
-from common import version
+from common.constant import FilePrefix, SystemString
+from common import preload, config, errorcode, version
 import routes
 
 
@@ -16,28 +13,25 @@ api = Api(app)
 routes.init(api)
 
 
-class Service(object):
-    def setup(self):
-        result_config = config.load(FilePrefix.CONFIG)
-        result_errorcode = errorcode.load(FilePrefix.ERRORCODE)
-        result_verison = version.load(FilePrefix.VERSION)
-        result_preload = preload.init()
+def setup():
+    try:
+        config.load(FilePrefix.CONFIG)
+        preload.init()
+        errorcode.load(FilePrefix.ERRORCODE)
+        version.load(FilePrefix.VERSION)
+    except IOError as e:
+        print(e)
+        sys.exit()
 
-        logging.getLogger().info(SystemString.PROJECT_NAME + " " + SystemString.PROJECT_VERSION)
-        logging.getLogger().info(result_config)
-        logging.getLogger().info(result_errorcode)
-        logging.getLogger().info(result_verison)
-        logging.getLogger().info(result_preload)
 
-    def finialize(self):
-        logging.getLogger().info(SystemString.PROJECT_NAME + " finished")
-
-    def run(self):
-        app.run()
+def run():
+    app.run(
+        host='0.0.0.0'
+    )
 
 
 if __name__ == '__main__':
-    service = Service()
-    service.setup()
-    service.run()
-    service.finialize()
+    setup()
+    logging.getLogger().info("%s %s", SystemString.PROJECT_NAME, SystemString.PROJECT_VERSION)
+    run()
+    logging.getLogger().info("%s finished", SystemString.PROJECT_NAME)
